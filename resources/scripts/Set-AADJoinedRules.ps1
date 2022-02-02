79a287f4d5a9f14a91e3c5d9dbf3ed06c9167e4f
Roberto Rodriguez <rrodri0622@gmail.com>
2022-02-02 11:55:01 -0500

    added SACL rules

---
+++ resources/scripts/Set-AADJoinedRules.ps1
@@ -0,0 +1,18 @@
+Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/OTRF/Set-AuditRule/master/Set-AuditRule.ps1')
+
+$AuditRules = @"
+regKey;wellKnownSidType;rights;inheritanceFlags;propagationFlags;auditFlags
+"HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Ngc\KeyTransportKey";"WorldSid";"ReadKey";"ContainerInherit";"InheritOnly";"Success"
+"HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin";"WorldSid";"ReadKey";"ContainerInherit";"InheritOnly";"Success"
+"@
+
+write-host "Enabling audit rules.."
+$AuditRules | ConvertFrom-Csv -Delimiter ';' | ForEach-Object {
+    if(!(Test-Path $_.regKey)){
+        Write-Host $_.regKey " does not exist.."
+    }
+    else {
+        Write-Host "Updating SACL of " $_.regKey
+        Set-AuditRule -RegistryPath $_.regKey -WellKnownSidType $_.wellKnownSidType -Rights $_.rights.split(",") -InheritanceFlags $_.inheritanceFlags -PropagationFlags $_.propagationFlags -AuditFlags $_.auditFlags -ErrorAction SilentlyContinue
+    }
+}
